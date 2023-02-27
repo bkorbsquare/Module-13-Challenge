59d70c246518681f383661efa98315969feb6293
Brandon <110788407+bkorbsquare@users.noreply.github.com>
2023-02-26 22:46:33 -0600

    routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { sequelize } = require('./models/Product');
 const routes = require('./routes');
+
+
 // import sequelize connection
 
 const app = express();
